4e21a81eb7a5612bc66cce242f1520651efba288
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2012-02-17 09:39:45 -0300

    Adding template

---
+++ template.rb
@@ -0,0 +1,33 @@
+run "rm -rf README public/index.html public/javascripts/* test app/views/layouts/*"
+
+gem "rspec"
+gem "rspec-rails"
+gem "factory_girl"
+gem "database_cleaner"
+gem "devise"
+gem "devise"
+gem "mysql2"
+gem "simplecov"
+run "bundle install"
+
+rake "db:create", :env => 'development'
+rake "db:create", :env => 'test'
+
+application  <<-GENERATORS
+config.generators do |g|
+  g.template_engine :erb
+  g.test_framework  :rspec, :fixture => true, :views => false
+  g.integration_tool :rspec, :fixture => true, :views => true
+  g.fixture_replacement :factory_girl, :dir => "spec/support/factories"
+end
+GENERATORS
+
+generate "rspec:install"
+generate "devise:install"
+inject_into_file 'spec/spec_helper.rb', "\nrequire 'factory_girl'", :after => "require 'rspec/rails'"
+
+git :init
+git :add => '.'
+git :commit => '-am "Initial commit"'
+
+puts "SUCCESS!"
